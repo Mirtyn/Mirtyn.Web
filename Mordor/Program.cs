ced97a27fd4ac8b8312cef014aa157330f3a1dd1
Steven Kimpe <steven.kimpe@tothedot.be>
2022-10-09 10:18:06 +0200

    test

--- Mordor/Program.cs
+++ Mordor/Program.cs
@@ -11,6 +11,7 @@ namespace Mirtyn.Mordor
             Random rndNames = new Random();
             Random rndTitles = new Random();
             Random rndLevel = new Random();
+            Random rndLevel2 = new Random();
 
             //for (uint ctr = 1; ctr <= 10; ctr++)
             //    Console.WriteLine($"{rnd.Next(),15:N0}");
