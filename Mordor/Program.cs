638a68e05815ff8ed679992a95f36c8153f74e1d
Mirtyn <mirtyn@mirtyn.be>
2022-10-10 18:40:41 +0200

    Development

--- Mordor/Program.cs
+++ Mordor/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading;
 
 namespace Mirtyn.Mordor
@@ -7,67 +8,198 @@ namespace Mirtyn.Mordor
     {
         static void Main(string[] args)
         {
-            Random rnd = new Random();
-            Random rndNames = new Random();
-            Random rndTitles = new Random();
-            Random rndLevel = new Random();
+            var OrcGenerator = new OrcGenerator();
 
-            //for (uint ctr = 1; ctr <= 10; ctr++)
-            //    Console.WriteLine($"{rnd.Next(),15:N0}");
-
-            var RandomSeed = (rnd.Next());
-
-            //UpperBoundRandoms(RandomSeed, 25);
-
-            string[] names = { "Ogg", "Ûkbûk", "Krimp", "Dûsh", "Goend", "Krûk" };
-            string[] titles = { "The Bloated", "The Fat", "Of The Hand", "The Dung Collector", "Black-Heart",
-                "The Blacksmith", "The Cannibal", "Dead Wood", "The Gab", "Itchy Feet",
-                "The Eater", "Child Eater", "The Mindless" };
-
-            int namesIndex = rndNames.Next(names.Length);
-            int titlesIndex = rndTitles.Next(titles.Length);
-
-            Console.WriteLine(names[namesIndex]);
-            Console.WriteLine(titles[titlesIndex]);
-
-            var Level = (rndLevel.Next(1,26));
-
-            //  Lvl         1    2-3   4-6   7-8   9-11  12-13 14-16 17-18 19-21 22-23 24-25
-            //  Num Traits  0    1     2     3     4     5     6     7     8     9     10
-
-            Console.WriteLine(Level);
-
-            string[] traits = { "" };
-
-            int LevelTraits = Convert.ToInt32(Level / 2.5);
-
-
-            for (int i = 0; i < LevelTraits; i++)
-            {
-                Console.WriteLine(LevelTraits);
-            }
-
-
-
-            //Console.WriteLine(RandomSeed);
-
-
+            OrcGenerator.OrcGeneratorStart();
+            
 
 
             Console.ReadKey();
         }
-        // Generate random numbers with an upper bound specified.
-        static void UpperBoundRandoms(int seed, int upper)
-        {
-            Console.WriteLine(
-                "\nRandom object, seed = {0}, upper bound = {1}:",
-                seed, upper);
-            Random randObj = new Random(seed);
+
         
-            // Generate six random integers from 0 to the upper bound.
-            for (int j = 0; j < 6; j++)
-                Console.Write("{0,11} ", randObj.Next(upper));
-            Console.WriteLine();
+
+
+    }
+    public class OrcCaptain
+    {
+        public string name = string.Empty;
+        public string title = string.Empty;
+        public int level = 0;
+        public float maxhealth = 0;
+        public float health = 0;
+        //public object traits = new List<string>();
+        public string[] traits = {
+            string.Empty,
+            string.Empty,
+            string.Empty,
+            string.Empty,
+            string.Empty,
+            string.Empty,
+            string.Empty,
+            string.Empty,
+            string.Empty,
+            string.Empty
+        };
+    }
+
+    public class OrcGenerator
+    {
+        public void OrcGeneratorStart()
+        {
+            Random rnd = new Random();
+
+            var orc = new OrcCaptain();
+
+            orc.name = OrcNameGenerator(rnd);
+
+            orc.title = OrcTitleGenerator(rnd);
+
+            orc.level = OrcLevelGenerator(rnd);
+
+            orc.maxhealth = OrcHealthGenerator(orc.level);
+
+            orc.health = orc.maxhealth;
+
+            orc.traits = OrcTraitsGenerator(rnd, orc.level);
+
+
+
+            DisplayOrc(orc, orc.traits);
+        }
+
+        public string OrcNameGenerator(Random rnd)
+        {
+            string[] names = {
+                "Ogg",
+                "Ûkbûk",
+                "Krimp",
+                "Dûsh",
+                "Goend",
+                "Krûk"
+            };
+            int namesIndex = rnd.Next(names.Length);
+            string name = (names[namesIndex]);
+
+            return name;
+        }
+
+        public string OrcTitleGenerator(Random rnd)
+        {
+            string[] titles = {
+                "The Bloated",
+                "The Fat",
+                "Of The Hand",
+                "The Dung Collector",
+                "Black-Heart",
+                "The Blacksmith",
+                "The Cannibal",
+                "Dead Wood",
+                "The Gab",
+                "Itchy Feet",
+                "The Eater",
+                "Child Eater",
+                "The Mindless"
+            };
+            int titlesIndex = rnd.Next(titles.Length);
+            string title = (titles[titlesIndex]);
+
+            return title;
+        }
+
+        public int OrcLevelGenerator(Random rnd)
+        {
+            var level = (rnd.Next(1, 26));
+
+            //  Lvl         1    2-3   4-6   7-8   9-11  12-13 14-16 17-18 19-21 22-23 24-25
+            //  Num Traits  0    1     2     3     4     5     6     7     8     9     10
+
+            return level;
+        }
+
+        public int OrcHealthGenerator(int level)
+        {
+            int maxhealth = level * 10;
+
+            return maxhealth;
+        }
+
+        public string[] OrcTraitsGenerator(Random rnd, int level)
+        {
+            //  Lvl         1    2-3   4-6   7-8   9-11  12-13 14-16 17-18 19-21 22-23 24-25
+            //  Num Traits  0    1     2     3     4     5     6     7     8     9     10
+
+            int LevelTraits = Convert.ToInt32(level / 2.5);
+
+            var TraitsList = new List<string> {
+                "Charge Attack",
+                "Rapid Attack",
+                "Leash Attack", 
+                "Jump Attack", 
+                "Axe Throw", 
+                "Stun Bombs", 
+                "Grab Throw",
+                "Ground Slam", 
+                "Body Slam",
+                "Vault Breaker",
+                "Regeneration",
+                "Gang Leader",
+                "Caragor Rider",
+                "Beast Slayer",
+                "Throwing Knifes"
+            };
+
+            if (LevelTraits > TraitsList.Count)
+            {
+                LevelTraits = TraitsList.Count;
+            }
+
+            var Traits = new List<string>();
+            var TraitsCopy = new List<string>(TraitsList);
+
+            for (var i = 0; Traits.Count < LevelTraits; i++)
+            {
+                var index = rnd.Next(TraitsCopy.Count);
+
+                var trait = TraitsCopy[index];
+
+                var exists = Traits.Exists(o => o == trait);
+
+                if (!exists)
+                {
+                    Traits.Add(trait);
+
+                    TraitsCopy.Remove(trait);
+                }
+            }
+            string[] TraitsArray = Traits.ToArray();
+
+            return TraitsArray;
+        }
+        
+        public void DisplayOrc(OrcCaptain orc, string[] traits)
+        {
+            Console.WriteLine("\nOrc captains name and title:");
+            Console.WriteLine(orc.name);
+            Console.WriteLine(orc.title);
+
+            Console.WriteLine("\nOrc captains level:");
+            Console.WriteLine(orc.level);
+
+            Console.WriteLine("\nOrc captains health:\n");
+            Console.WriteLine(orc.maxhealth);
+
+            Console.WriteLine("Orc captains traits:");
+            for (int i = 0; i < orc.traits.Length; i++)
+            {
+                Console.WriteLine(orc.traits[i]);
+            }
+            if (orc.traits.Length == 0)
+            {
+                Console.WriteLine("He doesn't have any traits.");
+            }
+
+            Console.WriteLine("\n\n");
         }
     }
 }
